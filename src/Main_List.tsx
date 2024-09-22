f19b2b9738c8d80a197e7d478d8726e63c48e392
rigra <kth5352@gmail.com>
2024-09-23 03:15:22 +0900

    0923

--- src/Main_List.tsx
+++ src/Main_List.tsx
@@ -47,34 +47,58 @@ function Main_List(): JSX.Element {
 
     return (
       <SafeAreaView style={styles.container}>
-        <FlatList style={{flex:1}}
-        data={callList}
-        ListHeaderComponent={Header}
-        renderItem={ListItem}
-        keyExtractor={(item:any)=>item.id} />
+        <FlatList
+          style={{flex: 1}}
+          data={callList}
+          ListHeaderComponent={Header}
+          renderItem={ListItem}
+          keyExtractor={(item: any) => item.id}
+        />
       </SafeAreaView>
     );
   };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-  },
-  button: {
-    width: '70%',
-    backgroundColor: '#3498db',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    textAlign: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+    },
+    button: {
+      width: '70%',
+      backgroundColor: '#3498db',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 16,
+      textAlign: 'center',
+    },
+    header: {
+      flexDirection: 'row',
+      height: 50,
+      marginBottom: 5,
+      backgroundColor: '#3498db',
+      color: 'white',
+      alignItems: 'center',
+    },
+    headerText: {
+      fontSize: 18,
+      textAlign: 'center',
+      color: 'white',
+    },
+    textForm: {
+      flex: 1,
+      borderWidth: 1,
+      borderColor: '#3498db',
+      height: hp(5),
+      paddingLeft: 10,
+      paddingRight: 10,
+    },
+  });
+}
 
 export default Main_List;
