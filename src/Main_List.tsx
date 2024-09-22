ba61dea659d002e4ac6484da3eaff4fd9fe0befc
rigra <kth5352@gmail.com>
2024-09-23 03:07:05 +0900

    0923

--- src/Main_List.tsx
+++ src/Main_List.tsx
@@ -1,24 +1,79 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
+import {SafeAreaView, StyleSheet, Text, View, FlatList} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {useState} from 'react';
+import {useFocusEffect} from '@react-navigation/native';
+import React from 'react';
 
 function Main_List(): JSX.Element {
-  console.log("--Main_List()");
+  console.log('--Main_List()');
 
-  return (
-    <SafeAreaView>
-      <Text style={styles.textBlack}>Hello ReactNative</Text>
-      <Text style={styles.textBlue}>Main_List</Text>
-    </SafeAreaView>
+  const [callList, setCallList] = useState([]);
+
+  const requestCallList = () => {
+    let tmp: any = [];
+    for (var i = 0; i < 10; i++) {
+      let row = {
+        id: i,
+        start_addr: '출발주소',
+        end_addr: '도착주소',
+        call_state: 'REQ',
+      };
+      tmp.push(row);
+    }
+  };
+
+  const Header = () => (
+    <View style={styles.header}>
+      <Text style={[styles.headerText, {width: wp(80)}]}>출발지 / 도착지</Text>
+      <Text style={[styles.headerText, {width: wp(20)}]}>상태</Text>
+    </View>
   );
-}
+
+  const ListItem = (row: {
+    item: {start_addr: string; end_addr: string; call_state: string};
+  }) => {
+    console.log('row = ' + JSON.stringify(row));
+
+    useFocusEffect(
+      React.useCallback(() => {
+        requestCallList();
+      }, []),
+    );
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList style={{flex:1}}
+        data={callList}
+        ListHeaderComponent={Header}
+        renderItem={ListItem}
+        keyExtractor={(item:any)=>item.id} />
+      </SafeAreaView>
+    );
+  };
 
 const styles = StyleSheet.create({
-  textBlack: {
-    fontSize: 18,
-    color: "black",
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
-  textBlue: {
-    fontSize: 18,
-    color: "blue",
+  button: {
+    width: '70%',
+    backgroundColor: '#3498db',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
 
